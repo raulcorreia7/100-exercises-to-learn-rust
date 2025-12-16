348d6b776667839d421a4e917d7fccaafbd3e814
Raul Correia <19838093+raulcorreia7@users.noreply.github.com>
2025-12-16 23:26:20 +0100

    feat(04_traits/12_copy): implement Copy trait for value types

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
-
+use std::ops::Add;
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +9,15 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl Add for WrappingU32 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        WrappingU32 {
+            value: self.value.wrapping_add(other.value),
+        }
     }
 }
 

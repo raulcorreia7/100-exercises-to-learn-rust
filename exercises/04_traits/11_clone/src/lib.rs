0d3dda970e92896ec917f5453e9a0ed9dfc7c05d
Raul Correia <19838093+raulcorreia7@users.noreply.github.com>
2025-12-16 23:26:17 +0100

    feat(04_traits/11_clone): implement Clone trait for custom structs

--- exercises/04_traits/11_clone/src/lib.rs
+++ exercises/04_traits/11_clone/src/lib.rs
@@ -2,9 +2,11 @@
 //  to get the code to compile.
 
 pub fn summary(ticket: Ticket) -> (Ticket, Summary) {
-    (ticket, ticket.summary())
+    let clone = ticket.clone();
+    (clone, ticket.summary())
 }
 
+#[derive(Clone)]
 pub struct Ticket {
     pub title: String,
     pub description: String,
@@ -19,7 +21,7 @@ impl Ticket {
         }
     }
 }
-
+#[derive(Clone)]
 pub struct Summary {
     pub title: String,
     pub status: String,

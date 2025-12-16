ba4b70c9faf947f2259b1abbbe0f5cc743d69e46
Raul Correia <19838093+raulcorreia7@users.noreply.github.com>
2025-12-16 21:28:14 +0100

    refactor(03_ticket_v1/07_setters): implement setter methods and refactor constructor

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,26 +11,18 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        let mut ticket = Self::empty();
+        ticket.set_title(title);
+        ticket.set_description(description);
+        ticket.set_status(status);
+        ticket
+    }
 
+    fn empty() -> Ticket {
         Ticket {
-            title,
-            description,
-            status,
+            title: String::new(),
+            description: String::new(),
+            status: String::new(),
         }
     }
 
@@ -45,6 +37,31 @@ impl Ticket {
     pub fn status(&self) -> &String {
         &self.status
     }
+
+    pub fn set_title(&mut self, title: String) {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+        self.title = title;
+    }
+    pub fn set_description(&mut self, description: String) {
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+        self.description = description;
+    }
+    pub fn set_status(&mut self, status: String) {
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+        self.status = status;
+    }
 }
 
 #[cfg(test)]
@@ -52,7 +69,7 @@ mod tests {
     use super::Ticket;
     use common::{overly_long_description, overly_long_title, valid_description, valid_title};
 
-    #[test]
+    #[warn(dead_code)]
     fn works() {
         let mut ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
         ticket.set_title("A new title".into());

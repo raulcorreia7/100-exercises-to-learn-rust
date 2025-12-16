643ac773492bce10f4f019b3657022d6f767f731
Raul Correia <19838093+raulcorreia7@users.noreply.github.com>
2025-12-16 23:30:30 +0100

    feat(04_traits/13_drop): implement Drop trait for resource cleanup

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,29 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+use core::panic;
+
+struct DropBomb {
+    defused: bool,
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.defused {
+            panic!("Bomb is not defused!")
+        }
+    }
+}
+
+impl DropBomb {
+    fn new() -> Self {
+        DropBomb { defused: false }
+    }
+
+    fn defuse(&mut self) {
+        self.defused = true;
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;

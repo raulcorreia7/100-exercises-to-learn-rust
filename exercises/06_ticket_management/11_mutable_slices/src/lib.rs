ad24532eeeeaaa8e106aea8255e27c9773a6b06e
Raúl Correia <19838093+raulcorreia7@users.noreply.github.com>
2025-12-21 00:12:32 +0000

    feat(exercises): solve 06_ticket_management/11_mutable_slices

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,9 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+pub fn squared(v: &mut [i32]) {
+    v.iter_mut().for_each(|x| *x *= *x);
+}
 #[cfg(test)]
 mod tests {
     use super::*;

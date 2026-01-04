6df00d4ada16d79315310c04d26ee0f2bd2fcee0
Raúl Correia <19838093+raulcorreia7@users.noreply.github.com>
2026-01-04 17:48:27 +0000

    feat(exercises): solve 08_futures/05_blocking

--- exercises/08_futures/05_blocking/src/lib.rs
+++ exercises/08_futures/05_blocking/src/lib.rs
@@ -8,11 +8,16 @@ use tokio::net::TcpListener;
 pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
     loop {
         let (socket, _) = listener.accept().await?;
-        let mut socket = socket.into_std()?;
-        socket.set_nonblocking(false)?;
-        let mut buffer = Vec::new();
-        socket.read_to_end(&mut buffer)?;
-        socket.write_all(&buffer)?;
+        let result = tokio::task::spawn_blocking(move || -> anyhow::Result<()> {
+            let mut socket = socket.into_std()?;
+            socket.set_nonblocking(false)?;
+            let mut buffer = Vec::new();
+            socket.read_to_end(&mut buffer)?;
+            socket.write_all(&buffer)?;
+            Ok(())
+        })
+        .await?;
+        result?;
     }
 }
 
